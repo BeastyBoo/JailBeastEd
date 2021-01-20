103c0ab4b06c9041f79b78f90ba65864905c8c06
BeastyBoo <torbjorn1.skavhaug@gmail.com>
2021-01-20 23:10:36 +0100

    Added easy accessible API usage.

--- src/main/java/com/github/beastyboo/jailbeasted/core/JailPlugin.java
+++ src/main/java/com/github/beastyboo/jailbeasted/core/JailPlugin.java
@@ -2,6 +2,8 @@ package com.github.beastyboo.jailbeasted.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class JailPlugin extends JavaPlugin {
 
     private JailCore core;
@@ -19,6 +21,9 @@ public final class JailPlugin extends JavaPlugin {
     }
 
     public JailAPI getAPI(){
+        if(core.getJailAPI() == null) {
+            this.getLogger().log(Level.WARNING, "Jail Beast Edition has not loaded yet!!");
+        }
         return core.getJailAPI();
     }
 
